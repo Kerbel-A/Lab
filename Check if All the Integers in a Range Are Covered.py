80defb35044bc38c25e1cdc015502d9673aacbd8
Kerbel-A <117370268+Kerbel-A@users.noreply.github.com>
2022-12-15 17:20:14 +0300

    Create Check if All the Integers in a Range Are Covered.py

---
+++ Check if All the Integers in a Range Are Covered.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/check-if-all-the-integers-in-a-range-are-covered/
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        diff = [0] * 52
+        for l, r in ranges:
+            diff[l] += 1
+            diff[r + 1] -= 1
+        cur = 0
+        for i, df in enumerate(diff):
+            cur += df
+            if left <= i <= right and cur == 0:
+                return False
+        return True
